ea2a4e399130cd6d8b7863ac5baa810745790bc5
Romeo-Peter <romeomeshack34@gmail.com>
2020-11-11 15:58:59 +0100

    Defined methods to interface with trading object and  to set up market activities time

--- robot/robot.py
+++ robot/robot.py
@@ -1,6 +1,6 @@
 from pprint import pprint
 
-import panda as pdb
+import pandas as pd
 
 from td.client import TDClient
 from td.utils import TDUtilities
@@ -30,13 +30,13 @@ class Robot:
         Paramenters
         -----------
         client-id: str
-            External API client id
+            External API consumer ID
         redirect_url: str
-            API fallbacl URL
+            API fallback URL
         credential_path: str, optional
             Path to API client credential
-        trading_account: str
-            Represents trading account
+        trading_account: str, optional
+            Trading account number
 
         Attributes
         ----------
@@ -51,7 +51,8 @@ class Robot:
 
         Returns
         -------
-        None: Nothing is returned
+        None
+            Nothing is returned
         """
         self.client_id: str = client_id
         self.redirect_url: str = redirect_url
@@ -62,13 +63,162 @@ class Robot:
         self.stock_frame = None
 
     def _create_session(self) -> TDClient:
+        """
+        Create session with stock API
+
+        Returns
+        -------
+        object
+            TDClient object with authenticated session
+        """
         td_api_client = TDClient(
             client_id=self.client_id,
             redirect_url=self.redirect_url,
-            credential_path=self.redential_path,
+            credential_path=self.credential_path,
         )
 
         # Start session
         td_api_client.login()
 
         return td_api_client
+
+    # NOTE: Search python @property
+    @property
+    def pre_market_open(self) -> bool:
+        """
+        Check for US market activities before actual market hours
+
+        Returns
+        -------
+        bool:
+            True if there's pre market, else false
+        """
+        pre_market_time = (
+            datetime.now()
+            .replace(hour=12, minute=00, second=00, tzinfo=timezone.utc)
+            .timestamp()
+        )
+        market_start_time = (
+            datetime.now()
+            .replace(hour=13, minute=30, second=00, tzinfo=timezone.utc)
+            .timestamp()
+        )
+        right_now = datetime.now().replace(tzinfo=timezone.utc).timestamp()
+
+        if market_start_time >= right_now >= pre_market_time:
+            return True
+        return False
+
+    @property
+    def post_market_open(self) -> bool:
+        """
+        Check for US market activities after actual market hours
+
+        Returns
+        -------
+        bool:
+            True if there's post market, else false
+        """
+        post_market_end_time = (
+            datetime.now()
+            .replace(hour=22, minute=30, second=00, tzinfo=timezone.utc)
+            .timestamp()
+        )
+        market_end_time = (
+            datetime.now()
+            .replace(hour=20, minute=30, second=00, tzinfo=timezone.utc)
+            .timestamp()
+        )
+        right_now = datetime.now().replace(tzinfo=timezone.utc).timestamp()
+
+        if post_market_end_time >= right_now >= market_end_time:
+            return True
+        return False
+
+    @property
+    def regular_market(self) -> bool:
+        """
+        Check for US stock market activities
+
+        Returns
+        -------
+        bool
+            True if there's market activities, else false
+        """
+        market_start_time = (
+            datetime.now()
+            .replace(hour=13, minute=30, second=00, tzinfo=timezone.utc)
+            .timestamp()
+        )
+        market_end_time = (
+            datetime.now()
+            .replace(hour=20, minute=30, second=00, tzinfo=timezone.utc)
+            .timestamp()
+        )
+        right_now = datetime.now().replace(tzinfo=timezone.utc).timestamp()
+
+        if market_end_time >= right_now >= market_start_time:
+            return True
+        return False
+
+    def portfolio(self):
+        """
+        Portfolio object store recognize and store positions
+
+        Returns
+        -------
+        Portfolio: object
+            portfolio object to store positions
+
+        """
+        pass
+
+    def trade(self):
+        """
+        Builds order with with pre-build templates that can be easily modified to include complex strategies.
+
+        Calls TD Ameritrade Quotes endpoint to get all
+        the positions in the portfolio. returns single dicitionary for one position, else a nested dictionary.
+
+        Returns
+        -------
+        Trade: object
+            Trade object with specified template
+        """
+        pass
+
+    def stock_frame():
+        """
+        Parameter
+        ---------
+        data: List[dict]
+            Data to add to stock frame object
+
+        Returns
+        -------
+        Stock-frane: object
+            Multi-indexed data-frame for trading
+        """
+        pass
+
+    def get_quotes() -> dict:
+        """
+        Get current quotes for all positions in portfolio
+
+        Returns
+        -------
+        dict
+            dictionary object containing all quotes and positions
+        """
+        pass
+
+    def get_historical_prices() -> List[Dict]:
+        """
+        Historical prices for positions in portfolio
+
+        Returns
+        -------
+        List[Dict]
+            The historical price candles.
+        """
+        pass

544956f7afe0018dff58d802c759c5aad167ed88
Romeo-Peter <romeomeshack34@gmail.com>
2020-11-20 02:51:48 +0100

    Grouping stocks data

--- robot/stock_frame.py
+++ robot/stock_frame.py
@@ -43,11 +43,39 @@ class StockFrame:
     @property
     def symbol_group(self) -> DataFrameGroupBy:
         """
-        Symbol groups in the StockFrame. The '_symbol_groups' property returns dataframe groups by each symbol
+        Returns symbol groups in the StockFrame. The '_symbol_groups' property returns dataframe groups by each symbol
 
         Returns
         -------
         DataFrameGroupBy
             A `pandas.core.groupby.GroupBy` object with each symbol.
         """
-        pass
+        self._symbol_groups = self._frame.groupby(
+            by="symbol", as_index=False, sort=True
+        )
+
+        return self._symbol_groups
+
+    def symbol_rolling_groups(self, size: int) -> RollingGroupby:
+        """
+        Grab the windows for each group
+
+        Parameter
+        ---------
+        size: int
+            Window size
+
+        Returns
+        -------
+        RollingGroupby
+            A `pandas.core.window.RollingGroupby` object.
+        """
+        if not self._symbol_groups:
+            self.symbol_group
+
+        self._symbol_rolling_groups = self._symbol_groups.rolling(size)
+
+        return self._symbol_rolling_groups
+
+    # NEXT: Create new data frame with data passs through
+    # TIMESTAMP: 10:59

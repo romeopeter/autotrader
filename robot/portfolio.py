7c9dc6f546ebf66a7173cd409a3ab3c2b3f60345
Romeo-Peter <romeomeshack34@gmail.com>
2020-12-03 08:37:11 +0100

    Updated comment documentation

--- robot/portfolio.py
+++ robot/portfolio.py
@@ -8,16 +8,17 @@ class Portfolio:
 
     def __init__(self, account_number: str = None):
         """
-        Initialies the stock portfolio object
+        Initialies new instance of the Portfolio object
 
         Parameters
         ---------
-        account_number: str
-            An account associated with portfolio
+        account_number: str, None
+            An account associated with portfolio. defaukt is set to nothing
         """
 
         self.positions = {}
         self.positions_count = 0
+
         self.market_value = 0.0
         self.profit_loss = 0.0
         self.risk_tolerance = 0.0
@@ -46,9 +47,9 @@ class Portfolio:
         quantity: int
             Quantity of shares or contracts owned
         purchase_price: float
-            Price of purchased position
+            Price at which the position was purchased (default: 0.00)
         purchase_date: str, optional
-            Date of purchased asset. Must be ISO format (YYYY-MM-DD):
+            Date at which the positioned was purchased. Must be ISO format (YYYY-MM-DD):
                 2020-11-14
 
         Return
@@ -56,7 +57,26 @@ class Portfolio:
         object: dict
             A dictionary object that represents a position in the portfolio.
 
+        Usage
+        -----
+        >>> portfolio = Portfolio()
+        >>> new_position = portfolio.add_position(symbol='TSLA',
+                asset_type='stocks',
+                quantity=2,
+                purchase_price=4.00,
+                purchase_date="2020-11-1"
+            )
+
+        >>> new_position
+        {
+            'asset_type': 'equity',
+            'quantity': 2,
+            'purchase_price': 4.00,
+            'symbol': 'MSFT',
+            'purchase_date': '2020-11-11'
+        }
         """
+
         self.positions[symbol] = {}
         self.positions[symbol]["symbol"] = symbol
         self.positions[symbol]["quantity"] = quantity
@@ -69,6 +89,7 @@ class Portfolio:
     def add_positions(self, positions: List(dict)) -> dict:
         """
         Add multiple positions to portfolio.
+
         This method take an interable of values and pass each position
         to the 'add_position' method
 
@@ -81,6 +102,44 @@ class Portfolio:
         -------
         dict
             Returns current position in portfolio
+
+        Usage
+        -----
+        >>> # Multiple position
+        >>> # multi_position = [
+            {
+            'asset_type': 'equity',
+            'quantity': 2,
+            'purchase_price': 4.00,
+            'symbol': 'TSLA',
+            'purchase_date': '2020-01-31'
+            },
+            {
+                'asset_type': 'equity',
+                'quantity': 2,0
+                'purchase_price': 4.00,
+                'symbol': 'AAPL',
+                'purchase_date': '2020-01-31'
+            }
+        ]
+
+        >>> new_positions = robot.portfolio.Portfolio.add_position(positions=multi_position)
+        {
+            'AAPL': {
+            'asset_type': 'equity',
+                'quantity': 2,0
+                'purchase_price': 4.00,
+                'symbol': 'AAPL',
+                'purchase_date': '2020-01-31'
+            },
+            'TSLA': {
+                'asset_type': 'equity',
+                'quantity': 2,
+                'purchase_price': 4.00,
+                'symbol': 'TSLA',
+                'purchase_date': '2020-01-31'
+            }
+        }
         """
 
         if isinstance(positions, list):
@@ -110,6 +169,22 @@ class Portfolio:
         Tuple: bool, str
             Returns True with messsage if deleted, otherwise False
             with message
+
+        Usage
+        -----
+        >>> portfolio = Portfolio()
+        >>> new_position = portfolio.add_position(
+                symbol='MSFT',
+                asset_type='equity',
+                quantity=2,
+                purchase_price=4.00,
+                purchase_date="2020-01-31"
+            )
+
+            >>> # Remove position
+            >>> delete_status = portfolio.remove_position(symbol='MSFT')
+            >>> delete_status
+            (True, 'MSFT was successfully removed')
         """
 
         if symbol in self.positions:
@@ -138,6 +213,22 @@ class Portfolio:
         return False
 
     def is_profitable(self, symbol: str, current_price: float) -> bool:
+        """Checks whether a position is profitable
+
+        Parameter
+        ---------
+        symbol: str
+            Symobol of the financial instrument used check profitability
+
+        current_price: float
+            Current trading rice of financial instrument
+
+        Return
+        -----
+        bool
+            Returns True if profitable, else false if flat
+        """
+
         if symbol in self.positions:
             # Grab purchase price
             purchase_price = self.positions[symbol].get("purchase_price", 0.0)
@@ -148,9 +239,7 @@ class Portfolio:
             return False
 
     def total_allocation(self):
-        """
-        Returns summary of portfolio by asset allocation
-        """
+        """Returns a summary of portfolio by asset allocation"""
         pass
 
     def risk_exposure(self):

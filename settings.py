9d279b06c1583be92fc5160f8639f4d03f98eaa4
Romeo-Peter <romeomeshack34@gmail.com>
2021-02-16 12:45:17 +0100

    Configurations

--- settings.py
+++ settings.py
@@ -5,6 +5,6 @@ DEBUG = config("DEBUG", cast=bool)
 
 # TDAmeritrade keys
 CLIENT_ID = config("CLIENT_ID", cast=str)
-REDIRECT_URL = config("REDIRECT_URL", cast=str)
+REDIRECT_URI = config("REDIRECT_URL", cast=str)
 JSON_PATH = config("JSON_PATH", cast=str)
 ACCOUNT_NUMBER = config("ACCOUNT_NUMBER", cast=str)

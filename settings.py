6beff868dd550358096f0f81fbecc66585f6aead
Romeo-Peter <romeomeshack34@gmail.com>
2021-02-18 16:04:36 +0100

    settings.py

--- settings.py
+++ settings.py
@@ -1,10 +1,9 @@
-# coding: utf-8
 from decouple import config
 
 DEBUG = config("DEBUG", cast=bool)
 
 # TDAmeritrade keys
 CLIENT_ID = config("CLIENT_ID", cast=str)
-REDIRECT_URI = config("REDIRECT_URL", cast=str)
+REDIRECT_URI = config("REDIRECT_URI", cast=str)
 JSON_PATH = config("JSON_PATH", cast=str)
 ACCOUNT_NUMBER = config("ACCOUNT_NUMBER", cast=str)

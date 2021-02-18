33bc9e91aa0fd729f0048f3065e1100384edda48
Romeo-Peter <romeomeshack34@gmail.com>
2021-02-18 15:45:07 +0100

    Created multi positions to be added to porfolio

--- run_robot.py
+++ run_robot.py
@@ -21,7 +21,31 @@ ACCOUNT_NUMBER = settings.ACCOUNT_NUMBER
 trading_robot = Robot(
     client_id=CLIENT_ID,
     redirect_uri=REDIRECT_URI,
-    credential_path=JSON_PATH,
+    credentials_path=JSON_PATH,
     trading_account=ACCOUNT_NUMBER,
     paper_trading=True,
 )
+
+# Create a new portfolio
+autotrader_portfolio = trading_robot.create_portfolio()
+
+# Add muultiple positions
+multi_positions = [
+    {
+        "asset_type": "equity",
+        "quantity": 2,
+        "purchase_price": 4.00,
+        "symbol": "TSLA",
+        "purchase_date": "2020-02-18",
+    },
+    {
+        "asset_type": "equity",
+        "quantity": 2,
+        "purchase_price": 4.00,
+        "symbol": "GME",
+        "purchase_date": "2020-02-18",
+    },
+]
+
+new_positions = autotrader_portfolio.add_positions(positions=multi_positions)
+print(new_positions)
